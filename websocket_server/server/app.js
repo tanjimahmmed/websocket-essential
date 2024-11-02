19b8f920951089368110b4b6d6af2f322cde329d
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-02 12:29:16 +0600

    create HTTP server

---
+++ websocket_server/server/app.js
@@ -0,0 +1,16 @@
+const HTTP = require('http')
+
+// import custom libary
+const CONSTANTS = require('./custom_lib/websocket_constants')
+const FUNCTION = require('./custom_lib/websocket_methods')
+
+// create a http web server obj
+const HTTP_SERVER = HTTP.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Hello we are live')
+})
+
+// HTTP => start the http serv
+HTTP_SERVER.listen(CONSTANTS.PORT, () => {
+    console.log('Sever is live on port: ' + CONSTANTS.PORT)
+})
